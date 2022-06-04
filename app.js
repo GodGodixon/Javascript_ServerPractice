d428c05a00b1b5279b434a00f422fe44c89aa70f
MoseOH <ohmose1004@gmail.com>
2022-06-04 10:52:28 +0900

    Practicing server - Day 1

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.set('port', process.env.PORT || '8080');
+
+app.get('/', (req, res) => {
+    res.send("nahz");
+
+});
+
+app.listen(app.get('port'), () => {
+
+    console.log('Port : ',  app.get('port'), ' had been connected.')
+})
