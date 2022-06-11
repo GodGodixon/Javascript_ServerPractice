ec0b7f42180192c36c81bbacd62cf56bb7ff3d2d
MoseOH <ohmose1004@gmail.com>
2022-06-11 14:42:43 +0900

    ERROR HELL HAHA

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ const express = require('express');
 const app = express();
 app.set('port', process.env.PORT || '8080');
 
+app.use((req, res, next) => {
+    console.log("You can see me easily :D");
+    next();
+})
+
 app.get('/', (req, res) => {
     res.send("nah");
+    console.log("Get RUNNED");
+});
 
+app.get('/error', (req, res) => {
+    res.send("ERROR AHHAHAHA");
+    console.log("Error here");
+    throw new Error("Error NOOOOOO");
+});
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'), () => {
 
     console.log('Port : ',  app.get('port'), ' had been connected.')
-})
+});
+
+//Middleware
