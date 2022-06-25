0eee36631e657c76d6d5b38b778fffc40fd3e02b
MoseOH <ohmose1004@gmail.com>
2022-06-25 09:37:35 +0900

    Cleaning bad things

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 
 const app = express();
-app.set('port', process.env.PORT || '8080');
+dotenv.config();
+app.set('port', process.env.PORT || '8081');
+
+
+app.use(morgan('dev'));
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret:process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly : true,
+        secure : false,
+    },
+
+
+}))
+
+app.use((req, res, next) => {
+    console.log('Run on ALL YEAH');
+    next();
+});
+
+app.get('/', (req, res, next) => {
+    console.log('GET 요청에서 실행되는 친구입니다');
+    res.status(200).send("Yeah good");
+})
+
+
 
 app.use((req, res, next) => {
     console.log("You can see me easily :D");
